2330f217b1036d3b27167d52e20f8186a0d59422
Andrew Lisowski <lisowski54@gmail.com>
2018-12-08 02:46:02 -0800

    add merged filter

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 npm i -g auto-release-cli
 
 mkdir ~/.ssh && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
 
 sh -c "auto $*"
