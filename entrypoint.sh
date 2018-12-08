c75755ae79cc65f1e51d2b9836104882f0dd7d33
Andrew Lisowski <lisowski54@gmail.com>
2018-12-08 02:07:49 -0800

    add npm token

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 set -e
 
 npm i -g auto-release-cli
-ls
+
+mkdir ~/.ssh && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
 sh -c "auto $*"
