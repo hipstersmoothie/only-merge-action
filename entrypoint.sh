d1b523da9bd7afe20cf4b2be137531f80ecc5679
Andrew Lisowski <lisowski54@gmail.com>
2018-12-10 23:32:48 -0800

    add token config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,12 @@ set -e
 cat $GITHUB_EVENT_PATH
 
 url=$(jq -r .repository.clone_url "$GITHUB_EVENT_PATH")
+
+git config --global user.name "Andrew Lisowski"
+git config --global user.email lisowski54@gmail.com 
+git config --global github.token $GITHUB_TOKEN
 git config --global http.sslVerify false
+
 git fetch origin
 # git branch --set-upstream master upstream/foo
 
