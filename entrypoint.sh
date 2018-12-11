a8dc6aa652ab67aaa30c4196abfb7df8fdf4b249
Andrew Lisowski <lisowski54@gmail.com>
2018-12-11 01:07:54 -0800

    add token config

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ git config --global http.sslVerify false
 git remote rm origin
 git remote add origin https://$GH_TOKEN@$url
 git fetch origin
-git branch --set-upstream master origin/master
+git branch --set-upstream-to origin/master
 
 git log -1 --pretty=%B | grep -F '[skip ci]' && exit 78
+
+exit 0
