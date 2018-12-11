91d056d7c74310b65d557c808085d26077c2d4b2
Andrew Lisowski <lisowski54@gmail.com>
2018-12-10 23:39:22 -0800

    add token config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ url=$(jq -r .repository.clone_url "$GITHUB_EVENT_PATH")
 
 git config --global github.user hipstersmoothie
 echo Set user
-git config --global github.token $GITHUB_TOKEN
+git config --global github.token $GH_TOKEN
 echo Set token
 git config --global http.sslVerify false
 echo Set ssl
